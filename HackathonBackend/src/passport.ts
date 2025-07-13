ede56ee1f6d0d9574ddbe24c90e9bfd42e627fb4
aankitkrr <ak1811103@gmail.com>
2025-07-13 14:38:04 +0530

    Fix: OAUTH redirecting uri

--- HackathonBackend/src/passport.ts
+++ HackathonBackend/src/passport.ts
@@ -9,7 +9,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID!,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    callbackURL: "/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL!,
   },
   async function (accessToken, refreshToken, profile, done) {
     try {
